0befe15bf20946b6abf54e63e67515c99c799f69
Alexey Danilov <Aleksey8205@yandex.ru>
2026-02-17 00:25:38 +0300

    release 1

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['public_html']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
